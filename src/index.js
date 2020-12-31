11d204a7ef7a2a17dbe352e344462c6f07aad861
Abdou <abdelmoula.chakhkhar@uit.ac.ma>
2020-12-31 20:50:46 +0100

    wlh ma3rft ach dart hna

--- src/index.js
+++ src/index.js
@@ -7,32 +7,6 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 5000;
 
-const multer = require("multer");
-const upload = multer({
-  dest: "images",
-  limits: {
-    fileSize: 1000000,
-  },
-  fileFilter(req, file, cb) {
-    // !file.originalname.match(/\.(doc|docx)$/))
-    if (!file.originalname.endsWith(".pdf")) {
-      return cb(new Error("Please upload a PDF"));
-    }
-    cb(undefined, true);
-  },
-});
-
-app.post(
-  "/upload",
-  upload.single("upload"),
-  (req, res) => {
-    res.send();
-  },
-  (error, req, res, newt) => {
-    res.status(400).send({ error: error.message });
-  }
-);
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const multer = require("multer");
+const sharp = require("sharp");
 const User = require("../models/user");
 const auth = require("../middleware/auth");
 const router = new express.Router();
@@ -101,7 +102,14 @@ router.post(
   auth,
   upload.single("avatar"),
   async (req, res) => {
-    req.user.avatar = req.file.buffer;
+    const buffer = await sharp(req.file.buffer)
+      .resize({
+        width: 250,
+        height: 250,
+      })
+      .png()
+      .toBuffer();
+    req.user.avatar = buffer;
     await req.user.save();
     res.send();
   },
@@ -114,6 +122,19 @@ router.delete("/users/me/avatar", auth, async (req, res) => {
   req.user.avatar = undefined;
   await req.user.save();
   res.send();
+});
+
+router.get("/users/:id/avatar", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user || !user.avatar) {
+      throw new Error();
+    }
+    res.set("Content-Type", "image/png");
+    res.send(user.avatar);
+  } catch (e) {
+    res.status(404).send();
+  }
 });
 
 module.exports = router;
