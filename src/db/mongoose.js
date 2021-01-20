d9729bd8d3dcfc9feb18554e464bbc71bd7c39e6
Abdou <abdelmoula.chakhkhar@uit.ac.ma>
2021-01-20 21:35:13 +0100

    testing with jest

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(`${process.env.MONGODB_URL}`, {
+// mongoose.connect(`${process.env.MONGODB_URL}`, {
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
---
+++ src/math.js
@@ -0,0 +1,27 @@
+const calculateTip = (total, tipPercent = 0.25) => total + total * tipPercent;
+
+const fahrenheightToCelsius = (temp) => {
+  return (temp - 32) / 1.8;
+};
+
+const celsiusToFahrenheit = (temp) => {
+  return temp * 1.8 + 32;
+};
+
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (a < 0 || b < 0) {
+        return reject("Numbers must be non-negative");
+      }
+      resolve(a + b);
+    }, 2000);
+  });
+};
+
+module.exports = {
+  calculateTip,
+  fahrenheightToCelsius,
+  celsiusToFahrenheit,
+  add,
+};
